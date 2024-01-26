8a0d8f6dab4f31819229dff1d42aac4e26eb7738
afuller87 <amyfuller19@gmail.com>
2024-01-26 14:42:33 -0500

    Final Updates

--- main.js
+++ main.js
@@ -8,10 +8,29 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // database connection
-mongoose.connect(process.env.DB_URI, {useNewParser: true});
+mongoose.connect(process.env.DB_URI);
 const db = mongoose.connection;
 db.on("error", (error) => console.log(error));
 db.once("open", ()=> console.log("Connected to the database!"));
+
+// middlewares
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.use(session({
+    secret: 'my secret key',
+    saveUnitialized: true,
+    resave: false,
+}));
+
+app.use((req, res, next) => {
+    res.locals.message = req.session.message;
+    delete req.session.message;
+    next();
+});
+
+// set template engine
+app.set("view engine", "ejs");
 
 
 app.get("/", (req, res) => {
