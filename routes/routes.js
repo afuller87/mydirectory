8a01f094687229e377b8d81d80811f5cc7c71983
afuller87 <amyfuller19@gmail.com>
2024-01-26 20:44:37 -0500

    Latest Updates

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,55 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/users');
+const multer = require('multer');
 
-router.get('/', (req, res) => {
-    res.render('index', { title: 'Home Page' });
+// image upload
+var storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, './uploads');
+    },
+    filename: function(req, file, cb){
+        cb(null, file.fieldname + '_' + Date.now() + '_' + file.originalname);
+    },
+});
+
+var upload = multer({
+    storage: storage,
+}).single('image');
+
+// insert a user into database route
+router.post('/add', upload, (req, res) => {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      image: req.file.filename,
+    });
+  
+    user.save()
+      .then(() => {
+        req.session.message = {
+          type: 'success',
+          message: 'User added successfully!',
+        };
+        res.redirect('/');
+      })
+      .catch(err => {
+        res.json({ message: err.message, type: 'danger' });
+      });
+  });
+
+// get all users route
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find().exec();
+        res.render('index', {
+            title: 'Home Page',
+            users: users,
+        });
+    } catch(err) {
+        res.json({ message: err.message });
+    }
 });
 
 router.get('/add', (req, res) => {
